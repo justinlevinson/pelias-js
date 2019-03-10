a135191de61240e88b906a863fc76cab5091b35a
justinlevinson <hello@levinsonjust.in>
2019-03-10 18:37:56 -0400

    Added focus search, return promise from search function

--- src/examples/text-search.ts
+++ src/examples/text-search.ts
@@ -4,4 +4,8 @@ var client = new Pelias()
 
 client.search
   .setSearchTerm('ymca')
+  .setFocusPoint("45.523064", "-122.676483")
   .execute()
+  .then((response) => {
+    console.log(response)
+  })
--- src/lib/search/index.ts
+++ src/lib/search/index.ts
@@ -1,12 +1,14 @@
 /**
  * A fluent interface for forward geocoding in Pelias
  */
-import { QS_TEXT } from '../constants'
+import { QS_TEXT, QS_FOCUS_LAT, QS_FOCUS_LONG } from '../constants'
 import { URLSearchParams } from "url";
 import { search } from '../fetch-utils/fetch'
 
 interface ISearchObject {
   searchTerm: string
+  focusPointLat: string
+  focusPointLong: string
 }
 
 // Auto-instantiate in case caller forgets 'new' so as not to pollute the global namespace
@@ -29,10 +31,17 @@ class Search {
     return this
   }
 
+  // Set a locale to search near - require both lat and long
+  setFocusPoint = (lat: string, long: string) => {
+    this._searchObject.focusPointLat = lat
+    this._searchObject.focusPointLong = long
+    return this
+  }
+
   execute = () => {
     const query = buildSearchQueryString(this._searchObject)
-    search(query).then((response) => {
-      console.log(response)
+    return search(query).then((response) => {
+      return(response)
     })
   }
 }
@@ -43,6 +52,11 @@ const buildSearchQueryString = (searchObject: ISearchObject) => {
 
   if(searchObject.searchTerm) {
     paramsArray.push([QS_TEXT, searchObject.searchTerm])
+  }
+
+  if(searchObject.focusPointLat && searchObject.focusPointLong) {
+    paramsArray.push([QS_FOCUS_LAT, searchObject.focusPointLat])
+    paramsArray.push([QS_FOCUS_LONG, searchObject.focusPointLong])
   }
 
   const searchParams = new URLSearchParams(paramsArray)
